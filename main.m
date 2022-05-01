7c5b0b5defd5d1b0ab6c6e32ed658dfb61d9053b
jab0073 <93546944+jab0073@users.noreply.github.com>
2022-04-30 21:26:35 -0500

    Add files via upload

---
+++ main.m
@@ -0,0 +1,31 @@
+N = 8;
+
+[img, ~, ~] = imread("GroupProject/pepper.png");
+
+yuvimg = rgb2ycbcr(img);
+
+imwrite(yuvimg, "pepperyuv.png");
+
+subsampledimage = FourTwoZeroSubSample(yuvimg);
+
+imwrite(subsampledimage, "peppersubbed.png");
+
+dctimg = DCT(subsampledimage, N);
+
+imwrite(dctimg,"pepperdct.png");
+
+quantizeimg = Quantize(dctimg, 100, N);
+
+imwrite(quantizeimg, "pepperquantize.png");
+
+dequantizedimg = Dequantize(quantizeimg, N);
+
+imwrite(quantizeimg, "pepperdequantize.png");
+
+idctimage = IDCT(dequantizedimg, N);
+
+imwrite(idctimage, "pepperidct.png");
+
+rgbimg = ycbcr2rgb(idctimage);
+
+imwrite(rgbimg,"pepperrgb.png");
