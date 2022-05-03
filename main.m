5e287b901436e1abd2594c1ce59c3bc1a0b6d264
jab0073 <93546944+jab0073@users.noreply.github.com>
2022-05-02 21:39:32 -0500

    Add files via upload

--- main.m
+++ main.m
@@ -1,85 +1,94 @@
 N = 8;
 
-% Pepper file
-[img, ~, ~] = imread("pepper.png");
+for filename = ["pepper.png","alu.tif"]
+    splitFileName = strsplit(filename,".");
+    % Pepper file
+    [img, ~, ~] = imread(filename);
+    
+    yuvimg = rgb2ycbcr(img);
+    
+    imwrite(yuvimg, strcat("OutputImgs/",splitFileName(1),"yuv.png"));
+    
+    subsampledimage = FourTwoZeroSubSample(yuvimg);
+    
+    imwrite(subsampledimage, strcat("OutputImgs/",splitFileName(1),"subbed.png"));
+    
+    dctimg = DCT(subsampledimage, N);
+    
+    imwrite(dctimg,strcat("OutputImgs/",splitFileName(1),"dct.png"));
+    
+    for qFactor = [5,50,100]
+    
+        quantizeimg = Quantize(dctimg, qFactor, N);
 
-yuvimg = rgb2ycbcr(img);
+        quantfile = sprintf("OutputImgs/%s_Quantized_Q-%d.png",splitFileName(1), qFactor);
+        imwrite(quantizeimg, quantfile);
+        
+        dequantizedimg = Dequantize(quantizeimg, N, qFactor);
+        
+        dequantfile = sprintf("OutputImgs/%s_Dequantized_Q-%d.png",splitFileName(1), qFactor);
+        imwrite(dequantizedimg, dequantfile);
+        
+        idctimage = IDCT(dequantizedimg, N);
+        
+        idctfile = sprintf("OutputImgs/%s_idct_Q-%d.png",splitFileName(1), qFactor);
+        imwrite(idctimage, idctfile);
+        
+        rgbimg = ycbcr2rgb(idctimage);
+    
+    
+        rgbfile = sprintf("OutputImgs/%s_rgb_Q-%d.png",splitFileName(1), qFactor);
+        imwrite(rgbimg,rgbfile);
+        
+        MSEvalue = MSE(img, rgbimg);
+        
+        disp(MSEvalue);
+        
+        psnrVal = PSNRvalue(double(MSEvalue));
+        
+        disp(psnrVal);
+    end
 
-imwrite(yuvimg, "pepperyuv.png");
-
-subsampledimage = FourTwoZeroSubSample(yuvimg);
-
-imwrite(subsampledimage, "peppersubbed.png");
-
-dctimg = DCT(subsampledimage, N);
-
-imwrite(dctimg,"pepperdct.png");
-
-quantizeimg = Quantize(dctimg, 100, N);
-
-imwrite(quantizeimg, "pepperquantize.png");
-
-dequantizedimg = Dequantize(quantizeimg, N, 100);
-
-imwrite(dequantizedimg, "pepperdequantize.png");
-
-idctimage = IDCT(dequantizedimg, N);
-
-imwrite(idctimage, "pepperidct.png");
-
-rgbimg = ycbcr2rgb(idctimage);
-
-
-imwrite(rgbimg,"pepperrgb.png");
-
-MSEvalue = MSE(img, rgbimg);
-
-disp(MSEvalue);
-
-psnrVal = PSNRvalue(double(MSEvalue));
-
-disp(psnrVal);
-
-
-
-
-% ALU.tif file
-
-[img, ~, ~] = imread("alu.tif");
-
-yuvimg = rgb2ycbcr(img);
-
-imwrite(yuvimg, "aluyuv.png");
-
-subsampledimage = FourTwoZeroSubSample(yuvimg);
-
-imwrite(subsampledimage, "alusubbed.png");
-
-dctimg = DCT(subsampledimage, N);
-
-imwrite(dctimg,"aludct.png");
-
-quantizeimg = Quantize(dctimg, 50, N);
-
-imwrite(quantizeimg, "aluquantize.png");
-
-dequantizedimg = Dequantize(quantizeimg, N, 50);
-
-imwrite(dequantizedimg, "aludequantize.png");
-
-idctimage = IDCT(dequantizedimg, N);
-
-imwrite(idctimage, "aluidct.png");
-
-rgbimg = ycbcr2rgb(idctimage);
+end
 
 
-imwrite(rgbimg,"alurgb.png");
-
-MSEvalue = MSE(img, rgbimg);
-
-disp(MSEvalue);
-
-psnrVal = PSNRvalue(double(MSEvalue));
-
-disp(psnrVal);
+% % ALU.tif file
+% 
+% [img, ~, ~] = imread("alu.tif");
+% 
+% yuvimg = rgb2ycbcr(img);
+% 
+% imwrite(yuvimg, "aluyuv.png");
+% 
+% subsampledimage = FourTwoZeroSubSample(yuvimg);
+% 
+% imwrite(subsampledimage, "alusubbed.png");
+% 
+% dctimg = DCT(subsampledimage, N);
+% 
+% imwrite(dctimg,"aludct.png");
+% 
+% quantizeimg = Quantize(dctimg, 50, N);
+% 
+% imwrite(quantizeimg, "aluquantize.png");
+% 
+% dequantizedimg = Dequantize(quantizeimg, N, 50);
+% 
+% imwrite(dequantizedimg, "aludequantize.png");
+% 
+% idctimage = IDCT(dequantizedimg, N);
+% 
+% imwrite(idctimage, "aluidct.png");
+% 
+% rgbimg = ycbcr2rgb(idctimage);
+% 
+% 
+% imwrite(rgbimg,"alurgb.png");
+% 
+% MSEvalue = MSE(img, rgbimg);
+% 
+% disp(MSEvalue);
+% 
+% psnrVal = PSNRvalue(double(MSEvalue));
+% 
+% disp(psnrVal);
